8bd65ca66a8abb2d55977f718b6be9aa62f1c517
Austin Schwartz <schwartzmas52@gmail.com>
2015-03-01 10:24:05 -0600

    var_dumps

--- add.php
+++ add.php
@@ -4,5 +4,7 @@
 		if(!is_resource($connection)){ echo 'Could not connect: '; print_r(sqlsrv_errors(SQLSRV_ERR_ALL)); }
 		$color = $_GET['color'];
 		$query = "INSERT INTO Colors (color) VALUES ($color)";
+		var_dump($query);
 		$result = sqlsrv_query($connection, $query);
+		var_dump($result);
 ?>
