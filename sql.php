7a7f0d68b9a5408bc138ef92facd78b27fe43675
Austin Schwartz <schwartzmas52@gmail.com>
2015-03-01 01:28:51 -0600

    var_dump 2

--- sql.php
+++ sql.php
@@ -2,6 +2,7 @@
 		$connectionInfo = array("Database"=>"iheartpaint", "UID"=>"iheartpaint@hackdfw", "PWD"=>"ilike2butts!");
 		$connection = sqlsrv_connect('hackdfw.database.windows.net', $connectionInfo);
 		$query = $_GET['query'];
+		var_dump($query);
 		$result = sqlsrv_query($connection, $query);
 		echo $result;
 		var_dump(sqlsrv_errors(SQLSRV_ERR_ALL));
