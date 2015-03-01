0711f48408639ac4864110d5a1bc11d51fe4a464
Austin Schwartz <schwartzmas52@gmail.com>
2015-03-01 05:25:51 -0600

    more var_dumps

--- delete.php
+++ delete.php
@@ -1,8 +1,9 @@
 <?php
 		$connectionInfo = array("Database"=>"iheartpaint", "UID"=>"hackdfw@hackdfw", "PWD"=>"ilike2butts!");
 		$connection = sqlsrv_connect('hackdfw.database.windows.net', $connectionInfo);
-		if(!is_resource($connection)){ echo 'Could not connect: '; print_r(sqlsrv_errors(SQLSRV_ERR_ALL)); }
+		//if(!is_resource($connection)){ echo 'Could not connect: '; print_r(sqlsrv_errors(SQLSRV_ERR_ALL)); }
 		$query = "DELETE FROM Colors WHERE (id = $_GET['id'])";
+		var_dump($query);
 		$result = sqlsrv_query($connection, $query);
 		var_dump($result);
 		header('Location: http://iheartpaint.azurewebsites.net/');
