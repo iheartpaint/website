3105ddd9b6e4dbf60fc27842a3daea6f769c1560
Crypteex <cryptex410@gmail.com>
2015-02-28 22:30:53 -0600

    fixed no android SDK

--- AndroidApp/app/src/main/java/hackdfw/androidapp/Colorscreen.java
+++ AndroidApp/app/src/main/java/hackdfw/androidapp/Colorscreen.java
@@ -3,8 +3,6 @@ package hackdfw.androidapp;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
