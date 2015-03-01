4c7c9b994283bf0e74ca825f08fc4543011f79fe
Austin Schwartz <schwartzmas52@gmail.com>
2015-03-01 01:04:09 -0600

    sql

--- sql.php
+++ sql.php
@@ -1,6 +1,6 @@
 <?php
 		$connectionInfo = array("Database"=>"iheartpaint", "UID"=>"iheartpaint@hackdfw", "PWD"=>"ilike2butts!");
 		$connection = sqlsrv_connect('hackdfw.database.windows.net', $connectionInfo);
-		$query = $_GET;
+		$query = $_GET['query'];
 		$result = sqlsrv_query($connection, $query);
 ?>
