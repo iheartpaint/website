190e8b2eb5861326f9bbcab1169fc941ae52f29e
Austin Schwartz <schwartzmas52@gmail.com>
2015-03-01 05:29:00 -0600

    cool

--- delete.php
+++ delete.php
@@ -1,4 +1,5 @@
 <?php
+		echo "cool";
 		$connectionInfo = array("Database"=>"iheartpaint", "UID"=>"hackdfw@hackdfw", "PWD"=>"ilike2butts!");
 		$connection = sqlsrv_connect('hackdfw.database.windows.net', $connectionInfo);
 		if(!is_resource($connection)){ echo 'Could not connect: '; print_r(sqlsrv_errors(SQLSRV_ERR_ALL)); }
