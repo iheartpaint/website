234a5d03fdd297e3c415538999b5ec23d94fd860
Austin Schwartz <schwartzmas52@gmail.com>
2015-03-01 05:27:09 -0600

    why?

--- delete.php
+++ delete.php
@@ -1,7 +1,8 @@
 <?php
 		$connectionInfo = array("Database"=>"iheartpaint", "UID"=>"hackdfw@hackdfw", "PWD"=>"ilike2butts!");
 		$connection = sqlsrv_connect('hackdfw.database.windows.net', $connectionInfo);
-		//if(!is_resource($connection)){ echo 'Could not connect: '; print_r(sqlsrv_errors(SQLSRV_ERR_ALL)); }
+		if(!is_resource($connection)){ echo 'Could not connect: '; print_r(sqlsrv_errors(SQLSRV_ERR_ALL)); }
+		echo "why?";
 		$query = "DELETE FROM Colors WHERE (id = $_GET['id'])";
 		var_dump($query);
 		$result = sqlsrv_query($connection, $query);
