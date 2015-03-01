47b0ef7ec20087d73514eb90915c03a4bb76482c
Austin Schwartz <schwartzmas52@gmail.com>
2015-03-01 04:04:35 -0600

    make colors work?

--- index.php
+++ index.php
@@ -7,26 +7,13 @@
 	<script type="text/javascript" src="js/bootstrap.min.js"></script>
 	<script type="text/javascript" src="js/ntc.js"></script>
 
-	<script>
+	<script type="text/javascript">
 <?php
 		$connectionInfo = array("Database"=>"iheartpaint", "UID"=>"hackdfw@hackdfw", "PWD"=>"ilike2butts!");
 		$connection = sqlsrv_connect('hackdfw.database.windows.net', $connectionInfo);
 		if(!is_resource($connection)){ echo 'Could not connect: '; print_r(sqlsrv_errors(SQLSRV_ERR_ALL)); }
 		$query = "SELECT id,color FROM Colors";
 		$result = sqlsrv_query($connection, $query);
-		if( $result === false) {
-			print_r( sqlsrv_errors(SQLSRV_ERR_ALL));
-		}
-		else
-		{
-			while($array = sqlsrv_fetch_array($result))
-			{
-?>
-				var color = ntc.name("#<? echo $array[1]; ?>");
-				$(".color-list").append("<div class='col-md-4 color'><div class='color-square' style='background-color:" + color[0] + "'></div><h3 class='color-name'>" + color[1] + "</h3> <div class='row'><div class='col-md-6'><div class='order btn btn-default'>Order</div></div><div class='col-md-6'><div class='delete btn btn-default'>Delete</div></div></div></div>");
-<?php
-			}
-		}
 ?>
 	</script>
 
@@ -81,7 +68,23 @@
 		</nav>
 
 		<div class="row color-list">
-
+<?php
+			if( $result === false) {
+					print_r( sqlsrv_errors(SQLSRV_ERR_ALL));
+			}
+			else
+			{
+				while($array = sqlsrv_fetch_array($result))
+				{
+?>
+					<script type="text/javascript">
+					var color = ntc.name("#<? echo $array[1]; ?>");
+					$(".color-list").append("<div class='col-md-4 color'><div class='color-square' style='background-color:" + color[0] + "'></div><h3 class='color-name'>" + color[1] + "</h3> <div class='row'><div class='col-md-6'><div class='order btn btn-default'>Order</div></div><div class='col-md-6'><div class='delete btn btn-default'>Delete</div></div></div></div>");
+					</script>
+<?php
+				}
+			}
+?>
 		</div>	
 
 	</div>
