3318173a9e2abcee0a25d7f84a7daa52e63d8624
Austin Schwartz <schwartzmas52@gmail.com>
2015-03-01 10:11:52 -0600

    add.php file

---
+++ add.php
@@ -0,0 +1,8 @@
+<?php
+		$connectionInfo = array("Database"=>"iheartpaint", "UID"=>"hackdfw@hackdfw", "PWD"=>"ilike2butts!");
+		$connection = sqlsrv_connect('hackdfw.database.windows.net', $connectionInfo);
+		if(!is_resource($connection)){ echo 'Could not connect: '; print_r(sqlsrv_errors(SQLSRV_ERR_ALL)); }
+		$color = $_GET['color'];
+		$query = "INSERT INTO Colors (color) VALUES ($color)";
+		$result = sqlsrv_query($connection, $query);
+?>
